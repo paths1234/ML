9e5ec7441b97310d610530d5e5d9e3f14f89a5f9
paths1234 <164713328+paths1234@users.noreply.github.com>
2025-02-01 14:46:49 +0700

    Add files via upload

---
+++ A4_T2_LM.py
@@ -0,0 +1,51 @@
+import sys
+
+with open(sys.argv[1], 'r') as file:
+    input = file.read()
+    line = input.strip().split('\n')
+
+n_list = []
+gr = {}
+gr_index = 1
+n_line = 0
+
+while n_line < len(line) - 1:
+  gr_order = int(line[n_line])
+
+  for i in range(n_line + 1, n_line + gr_order + 1):
+    n_list.append([int(n) for n in line[i].split()])
+
+  gr[gr_index] = n_list
+  n_list = []
+  gr_index += 1
+  n_line += gr_order + 1
+
+def largest_component(gr):
+    n_node = len(gr)
+    visit = [False] * n_node
+    component_order = []
+
+    for node in range(n_node):
+        if not visit[node]:
+            order = dfs(node, gr, visit)
+            component_order.append(order)
+
+    return max(component_order)
+
+def dfs(node, gr, visited):
+    s = [node]
+    component_order = 0
+
+    while s:
+        a = s.pop()
+        if not visited[a]:
+            visited[a] = True
+            component_order += 1
+            for i in gr[a]:
+                s.append(i)
+
+    return component_order
+
+for i in gr.keys():
+  max_order = largest_component(gr[i])
+  print("Graph {} has a component of order {}.".format(i, max_order))
